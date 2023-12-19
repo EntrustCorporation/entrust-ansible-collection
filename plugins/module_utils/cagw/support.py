2eaeaf8f949e0948695d2fd737bbbee3add5ef70
eve <eve@dmspsAnsible18.idm.dev.entrust.com>
2023-12-18 19:25:18 -0600

    Ansible-lint changes

--- plugins/module_utils/cagw/support.py
+++ plugins/module_utils/cagw/support.py
@@ -6,6 +6,8 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import traceback
+
 CRYPTOGRAPHY_IMP_ERR = None
 try:
     from cryptography import x509
